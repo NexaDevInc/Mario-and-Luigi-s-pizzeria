dd24e45d904fb5bc85f52b45cec6e75c95ec6b7a
BramHoekstra <bramhoekstra95@gmail.com>
2024-11-04 20:10:10 +0100

    Made the checkbox work and let changed the styling based on if the checkbox. Also made the address and billing address inputs work based on the checkbox.

--- static/js/functions_delivery.js
+++ static/js/functions_delivery.js
@@ -5,3 +5,40 @@ generateTimeSlots(openingTime, closingTime).forEach(slot => {
     option.textContent = `${slot.time}`;
     deliveryTimeSlotSelect.appendChild(option);
 });
+
+const checkbox = document.getElementById('checkbox');
+const address = document.getElementById('address');
+const billingAddress = document.getElementById('billing-address');
+const billingAddressLabel = document.getElementById('billing-address-label');
+let tempBillingAddress = '';
+
+address.addEventListener('input', () => {
+    if (checkbox.checked) {
+        billingAddress.value = address.value;
+    }
+});
+
+billingAddress.addEventListener('input', () => {
+    if (checkbox.checked) {
+        billingAddress.disabled = true;
+    } else {
+        tempBillingAddress = billingAddress.value;
+        billingAddress.disabled = false;
+    }
+});
+
+checkbox.addEventListener('click', () => {
+    if (checkbox.checked) {
+        billingAddressLabel.style.color = '#a1a1a1';
+        billingAddress.style.color = '#616161';
+        billingAddress.style.backgroundColor = '#a1a1a1';
+        billingAddress.value = address.value;
+        billingAddress.disabled = true;
+    } else {
+        billingAddressLabel.style.color = '#F0EFF4';
+        billingAddress.style.color = '#58641D';
+        billingAddress.style.backgroundColor = '#F0EFF4';
+        billingAddress.value = tempBillingAddress;
+        billingAddress.disabled = false;
+    }
+});
