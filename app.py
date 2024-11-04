3aa8e610348f1cacfe15f33ed76a238b3394e51b
BramHoekstra <bramhoekstra95@gmail.com>
2024-11-04 11:51:15 +0100

    add login and register pages with styling and changed styling to delivery and take-out pages. I also renamed some files and referenced them in the corresponding files accordingly.

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/login')
+def show_loginPage():
+    return render_template("login.html")
+
+@app.route('/register')
+def show_registerPage():
+    return render_template("register.html")
+
 @app.route('/delivery')
 def show_deliveryPage():
     return render_template("delivery.html")
