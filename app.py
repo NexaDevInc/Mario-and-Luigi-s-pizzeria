fa01343e153fe1b2903e1eccc0fe5fd7f175108d
BramHoekstra <bramhoekstra95@gmail.com>
2024-11-05 10:16:41 +0100

    added functions to app.py to save the inputs from the delivery and take-out pages to a json file called orders.json

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
+import json
 from Menu import Menu_Blueprint
 from LoginRegister import LoginRegister_Blueprint
 app = Flask(__name__)
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+
+json_file_path = os.path.join(DATA_DIR, 'orders.json')
+
+def saveData_JSON(orders):
+    with open(json_file_path, 'w') as json_file:
+        json.dump(orders, json_file)
+        
+def load_data_from_json(): 
+    if os.path.exists(json_file_path):
+            with open(json_file_path, 'r') as json_file:
+                content = json_file.read().strip()  
+                if not content:  
+                    return [] 
+                fromJson = json.loads(content)
+                return fromJson
+            return []
 
 app.secret_key = "TopSecretKey"
 
@@ -23,9 +46,39 @@ app.register_blueprint(LoginRegister_Blueprint)
 def show_deliveryPage():
     return render_template("delivery.html")
 
+@app.route('/delivery', methods = ['POST'])
+def get_delivery_form_values():
+    deliveryName = request.form.get["name"]
+    deliveryAddress = request.form.get["address"]
+    deliveryBillingAddress = request.form.get["billing-address"]
+    deliveryZipCode = request.form.get["zip-code"]
+    deliveryTime = request.form.get["delivery-time"]
+    deliveryPizzaChoice = request.form.get["pizza-choice"]
+
+    orders = load_data_from_json()
+    
+    newOrder = {'name': deliveryName, 'address': deliveryAddress, 'billing-address': deliveryBillingAddress, 'zip-code': deliveryZipCode, 'time': deliveryTime, 'pizza-choice': deliveryPizzaChoice}
+    orders.append(newOrder)
+
+    saveData_JSON(orders)
+
 @app.route('/take-out')
 def show_takeoutPage():
     return render_template("take-out.html")
+
+@app.route('/take-out', methods = ['POST'])
+def get_take_out_form_values():
+    takeOutName = request.form.get["name"]
+    takeOutBillingAddress = request.form.get["billing-address"]
+    takeOutTime = request.form.get["take-out-time"]
+    takeOutPizzaChoice = request.form.get["pizza-choice"]
+
+    orders = load_data_from_json()
+    
+    newOrder = {'name': takeOutName, 'address': '', 'billing-address': takeOutBillingAddress, 'zip-code': '', 'time': takeOutTime, 'pizza-choice': takeOutPizzaChoice}
+    orders.append(newOrder)
+
+    saveData_JSON(orders)
 
 @app.route('/home')
 def show_home():
